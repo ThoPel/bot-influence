9ce436cee634b4af5a1186bd66b83b89d194703a
PELFRENE <87179999+ThoPel@users.noreply.github.com>
2023-03-04 12:42:45 +0100

    feat : change dialog

--- src/index.js
+++ src/index.js
@@ -138,7 +138,7 @@ client.on('interactionCreate', (interraction) => {
         })
         if (result) {
             if (result.sender.user.id != interraction.member.id) {
-                interraction.reply(`❌ ${interraction.member} refuse le duel contre ${result.sender} !\nIl/Elle a peut-être une peur irrationnelle des petites balles en plastique et des joueurs en bois qui le regardent avec leurs yeux noirs effrayants ! Ou peut-être qu'il/elle a peur de perdre et devoir faire la vaisselle pour le reste de la semaine...`)
+                interraction.reply(`❌ ${interraction.member} refuse le duel contre ${result.sender} !\nIl/Elle a peut-être une peur irrationnelle des petites balles ! Ou peut-être qu'il/elle a peur de perdre et devoir faire la vaisselle pour le reste de la semaine...`)
                 const index = duels.indexOf(elem => elem.target.Discriminator == interraction.member.Discriminator);
                 duels.splice(index, 1);
 
@@ -152,12 +152,14 @@ client.on('interactionCreate', (interraction) => {
 
     // ECOUTEURS
     if (interraction.commandName === 'headset') {
-        if (!interraction.member.roles.cache.has('1079837778440896653')) {
-            interraction.member.roles.add(['1079837778440896653'], `${interraction.user.username} a mis ses écouteurs.`);
-            interraction.reply({ content: '🔈 J\'indique aux autres que tu as mis tes écouteurs ! ', ephemeral: true })
-        } else {
-            interraction.member.roles.remove(['1079837778440896653'], `${interraction.user.username} a retiré ses écouteurs.`);
+        const role = client.guilds.cache.get(process.env.GUILD_ID).roles.cache.find(role => role.name === '🎧 Écouteurs');
+        const hasHeadset = interraction.member.roles.cache.some(role => role.name === '🎧 Écouteurs');
+        if (hasHeadset) {
+            interraction.member.roles.remove(role);
             interraction.reply({ content: '🔇 J\'indique aux autres que tu as retiré tes écouteurs !', ephemeral: true });
+        } else {
+            interraction.member.roles.add(role);
+            interraction.reply({ content: '🔈 J\'indique aux autres que tu as mis tes écouteurs ! ', ephemeral: true })
         }
     }
 });
